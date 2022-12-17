abc1d6a20780832252c1008778cb184e7cb68f2e
cypress-krishna <cypressbatch2@gmail.com>
2022-12-17 17:21:15 +0530

    cypress hooks test case

---
+++ cypress/e2e/cypress-hooks/cypress-hooks.cy.js
@@ -0,0 +1,10 @@
+/// <reference types = "cypress" />
+
+describe('Cypress Hooks Concept', ()=> {
+
+    it("test case for cypress hooks", ()=> {
+        cy.log('cypress hooks...')
+    })
+});
+
+    
